17d9af5ebdbadabb91ce3e3bd9881028ecc360a3
RuiHanDing <49255738+RuiHanDing@users.noreply.github.com>
2019-05-29 06:14:39 +1000

    Home controller

---
+++ HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Insight_System.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Go()
+        {
+            ViewBag.Message = "Look Up Feature Places.";
+
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            ViewBag.Message = "Search Your Own Journey.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Showing Members.";
+
+            return View();
+        }
+
+
+    }
+}
